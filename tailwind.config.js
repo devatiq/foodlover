9d1ea14c9c63a32b9f5556e52f5a672ebce76477
devatiq <devatiqbd@gmail.com>
2021-10-17 17:24:19 +0600

    Tailwind CSS part 4

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fc1f15',
+        secondary: {
+          100: '#8d98ff',
+          200: '#636bff',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
